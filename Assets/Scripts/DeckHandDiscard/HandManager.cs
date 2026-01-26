7118ae4f018e9595db9dc42bd52f1d24ad396831
Inari <pusa.inari@gmail.com>
2026-01-26 11:33:47 +0200

    First clean up after card movement changes

--- Assets/Scripts/DeckHandDiscard/HandManager.cs
+++ Assets/Scripts/DeckHandDiscard/HandManager.cs
@@ -36,29 +36,13 @@ public class HandManager : MonoBehaviour
     {
         if (cardsInHand.Count < handMaxSize)
         {
+            GameObject newCard = Instantiate(cardPrefab, handTransform.position, Quaternion.identity, handTransform);
 
-            //Instatiate the card
-            if (usingNewSystem)
-            {
-                GameObject newCard = Instantiate(newCardPrefab, handTransform.position, Quaternion.identity, handTransform);
+            //Set the CardData of the instantieted card
 
-                //Set the CardData of the instantieted card
-
-                newCard.GetComponent<Card>().cardData = cardData;
-                newCard.GetComponent<Card>().UpdateCardDisplay();
-                RegisterCard(newCard);
-            }
-            else
-            {
-                GameObject newCard = Instantiate(cardPrefab, handTransform.position, Quaternion.identity, handTransform);
-                cardsInHand.Add(newCard);
-
-                //Set the CardData of the instantieted card
-
-                newCard.GetComponent<Card>().cardData = cardData;
-                newCard.GetComponent<Card>().UpdateCardDisplay();
-                UpdateHandVisuals();
-            } 
+            newCard.GetComponent<Card>().cardData = cardData;
+            newCard.GetComponent<Card>().UpdateCardDisplay();
+            RegisterCard(newCard);  
         }        
     }
 
@@ -70,116 +54,33 @@ public class HandManager : MonoBehaviour
 
     public void UpdateHandVisuals()
     {
-        if (usingNewSystem)
-        {
-            int cardCount = cardsInHand.Count;
-            if (cardCount == 0) return;
 
-            if (cardCount == 1)
-            {
-                var layout = cardsInHand[0].GetComponent<CardHandLayout>();
-                layout.SetTarget(Vector3.zero, Quaternion.identity);
-                return;
-            }
-
-            float centerOffset = (cardCount - 1) / 2f;
-
-            for (int i = 0; i < cardCount; i++)
-            {
-                float rot = fanSpread * (i - centerOffset);
-                float x = cardSpacing * (i - centerOffset);
-
-                float t = (2f * i / (cardCount - 1) - 1f);
-                float y = verticalSpacing * (1 - t * t);
-
-                var layout = cardsInHand[i].GetComponent<CardHandLayout>();
-                layout.SetTarget(new Vector3(x, y, 0f), Quaternion.Euler(0, 0, rot));
-            }
-        }
-        else OldUpdateVisuals();
-    }
-
-    public void UpdateHovered(GameObject hoveredCard)
-    {
-        if (usingNewSystem)
-        {
-            int cardCount = cardsInHand.Count;
-            if (cardCount == 0) return;
-
-            float pushAmount = 60f;
-            float hoverRise = 60f;
-
-            int hoveredIndex = cardsInHand.IndexOf(hoveredCard);
-            if (hoveredIndex < 0) return;
-
-            // Single card
-            if (cardCount == 1)
-            {
-                var layout = cardsInHand[0].GetComponent<CardHandLayout>();
-                layout.SetTarget(new Vector3(0f, hoverRise, 0f), Quaternion.identity);
-                return;
-            }
-
-            float centerOffset = (cardCount - 1) / 2f;
-
-            for (int i = 0; i < cardCount; i++)
-            {
-                float rot = fanSpread * (i - centerOffset);
-                float x = cardSpacing * (i - centerOffset);
-
-                float t = (2f * i / (cardCount - 1) - 1f);
-                float y = verticalSpacing * (1 - t * t);
-
-                if (i == hoveredIndex)
-                {
-                    y += hoverRise;
-                    rot = 0f;
-                }
-                else
-                {
-                    if (i < hoveredIndex) x -= pushAmount;
-                    else if (i > hoveredIndex) x += pushAmount;
-                }
-
-                var layout = cardsInHand[i].GetComponent<CardHandLayout>();
-                layout.SetTarget(new Vector3(x, y, 0f), Quaternion.Euler(0, 0, rot));
-            }
-        }
-        else OldUpdateHovered(hoveredCard);
-    }
-
-    public void OldUpdateVisuals()
-    {
         int cardCount = cardsInHand.Count;
-
         if (cardCount == 0) return;
 
-        // Special case: single card
         if (cardCount == 1)
         {
-            CardMovement cm = cardsInHand[0].GetComponent<CardMovement>();
-            cm.targetHandPos = Vector3.zero;
-            cm.targetHandRot = Quaternion.identity;
+            var layout = cardsInHand[0].GetComponent<CardHandLayout>();
+            layout.SetTarget(Vector3.zero, Quaternion.identity);
             return;
         }
 
+        float centerOffset = (cardCount - 1) / 2f;
+
         for (int i = 0; i < cardCount; i++)
         {
-            float centerOffset = (cardCount - 1) / 2f;
-
             float rot = fanSpread * (i - centerOffset);
             float x = cardSpacing * (i - centerOffset);
 
             float t = (2f * i / (cardCount - 1) - 1f);
             float y = verticalSpacing * (1 - t * t);
 
-            CardMovement cm = cardsInHand[i].GetComponent<CardMovement>();
-            cm.targetHandPos = new Vector3(x, y, 0f);
-            cm.targetHandRot = Quaternion.Euler(0, 0, rot);
+            var layout = cardsInHand[i].GetComponent<CardHandLayout>();
+            layout.SetTarget(new Vector3(x, y, 0f), Quaternion.Euler(0, 0, rot));
         }
     }
 
-    public void OldUpdateHovered(GameObject hoveredCard)
+    public void UpdateHovered(GameObject hoveredCard)
     {
         int cardCount = cardsInHand.Count;
         if (cardCount == 0) return;
@@ -188,16 +89,13 @@ public class HandManager : MonoBehaviour
         float hoverRise = 60f;
 
         int hoveredIndex = cardsInHand.IndexOf(hoveredCard);
-        if (hoveredIndex < 0) hoveredIndex = 0;
+        if (hoveredIndex < 0) return;
 
-        // 🔹 SPECIAL CASE: only one card
+        // Single card
         if (cardCount == 1)
         {
-            CardMovement cm = cardsInHand[0].GetComponent<CardMovement>();
-
-            cm.targetHandPos = new Vector3(0f, hoverRise, 0f);
-            cm.targetHandRot = Quaternion.identity;
-
+            var layout = cardsInHand[0].GetComponent<CardHandLayout>();
+            layout.SetTarget(new Vector3(0f, hoverRise, 0f), Quaternion.identity);
             return;
         }
 
@@ -205,9 +103,6 @@ public class HandManager : MonoBehaviour
 
         for (int i = 0; i < cardCount; i++)
         {
-            GameObject cardObj = cardsInHand[i];
-            CardMovement cm = cardObj.GetComponent<CardMovement>();
-
             float rot = fanSpread * (i - centerOffset);
             float x = cardSpacing * (i - centerOffset);
 
@@ -217,20 +112,18 @@ public class HandManager : MonoBehaviour
             if (i == hoveredIndex)
             {
                 y += hoverRise;
-                cm.targetHandRot = Quaternion.identity;
+                rot = 0f;
             }
             else
             {
                 if (i < hoveredIndex) x -= pushAmount;
                 else if (i > hoveredIndex) x += pushAmount;
-
-                cm.targetHandRot = Quaternion.Euler(0f, 0f, rot);
             }
 
-            cm.targetHandPos = new Vector3(x, y, 0f);
+            var layout = cardsInHand[i].GetComponent<CardHandLayout>();
+            layout.SetTarget(new Vector3(x, y, 0f), Quaternion.Euler(0, 0, rot));
         }
     }
-
 
     public void RegisterCard(GameObject card)
     {
